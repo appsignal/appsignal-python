976430d0634509b80821a653c208e43a3a51c4e6
Noemi <45180344+unflxw@users.noreply.github.com>
2023-03-16 17:20:01 +0100

    Disable source distribution builds
    As it does not make sense to build a source distribution package for
    this project, since it depends on the platform-specific agent, add
    a build hook for source distributions that always fails.

---
+++ src/scripts/sdist_hook.py
@@ -0,0 +1,16 @@
+from typing import Any
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+class CustomBuildHook(BuildHookInterface):
+    def initialize(self, version: str, build_data: dict[str, Any]) -> None:
+        """
+        This occurs immediately before each build.
+
+        Any modifications to the build data will be seen by the build target.
+        """
+        print(
+          "It is not possible to build a source distribution package for "
+          "this project, as it requires a platform-dependent binary."
+        )
+        print("Hint: you may wish to run `hatch run build:all` instead?")
+        exit(1)
