95dfe815766e0fd49003c3621b38a89315215c3b
Tom de Bruijn <tom@tomdebruijn.com>
2023-05-01 11:45:49 +0200

    Add boolean config options
    Add config options that are booleans and should be interpreted as such.

--- src/appsignal/client.py
+++ src/appsignal/client.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 from .agent import start_agent
-from .config import from_system, from_public_environ
+from .config import DEFAULT_CONFIG, from_system, from_public_environ
 from .opentelemetry import start_opentelemetry
 
 if TYPE_CHECKING:
@@ -13,7 +13,7 @@ class Client:
     _options: Options
 
     def __init__(self, **options: Unpack[Options]):
-        self._options = from_system() | options | from_public_environ()
+        self._options = DEFAULT_CONFIG | from_system() | options | from_public_environ()
 
     def start(self):
         start_agent(self._options)
--- tests/test_config.py
+++ tests/test_config.py
@@ -16,23 +16,63 @@ def test_from_system():
 
 
 def test_from_public_environ():
+    os.environ["APPSIGNAL_ACTIVE"] = "true"
     os.environ["APPSIGNAL_APP_ENV"] = "development"
     os.environ["APPSIGNAL_APP_NAME"] = "MyApp"
+    os.environ["APPSIGNAL_ENABLE_HOST_METRICS"] = "true"
+    os.environ["APPSIGNAL_ENABLE_NGINX_METRICS"] = "false"
+    os.environ["APPSIGNAL_ENABLE_STATSD"] = "false"
+    os.environ["APPSIGNAL_FILES_WORLD_ACCESSIBLE"] = "true"
     os.environ["APPSIGNAL_HOSTNAME"] = "Test hostname"
     os.environ["APPSIGNAL_LOG_LEVEL"] = "trace"
     os.environ["APPSIGNAL_PUSH_API_KEY"] = "some-api-key"
+    os.environ["APPSIGNAL_RUNNING_IN_CONTAINER"] = "true"
+    os.environ["APPSIGNAL_SEND_ENVIRONMENT_METADATA"] = "true"
+    os.environ["APPSIGNAL_SEND_PARAMS"] = "true"
+    os.environ["APPSIGNAL_SEND_SESSION_DATA"] = "true"
     os.environ["APP_REVISION"] = "abc123"
 
     config = from_public_environ()
 
     assert config == Options(
+        active=True,
+        enable_host_metrics=True,
+        enable_nginx_metrics=False,
+        enable_statsd=False,
         environment="development",
+        files_world_accessible=True,
         hostname="Test hostname",
         log_level="trace",
         name="MyApp",
         push_api_key="some-api-key",
         revision="abc123",
+        running_in_container=True,
+        send_environment_metadata=True,
+        send_params=True,
+        send_session_data=True,
     )
+
+
+def test_from_public_environ_bool_is_unset():
+    config = from_public_environ()
+
+    assert config.get("active") is None
+
+
+def test_from_public_environ_bool_is_empty_string():
+    os.environ["APPSIGNAL_ACTIVE"] = ""
+
+    config = from_public_environ()
+
+    assert config.get("active") is None
+
+
+def test_from_public_environ_bool_is_invalid():
+    os.environ["APPSIGNAL_ACTIVE"] = "invalid"
+
+    config = from_public_environ()
+
+    assert config.get("active") is None
 
 
 def test_from_public_environ_disable_default_instrumentations_list():
@@ -61,24 +101,52 @@ def test_from_public_environ_disable_default_instrumentations_bool():
 
 def test_set_private_environ():
     config = Options(
+        active=True,
         app_path="/path/to/app",
+        enable_host_metrics=True,
+        enable_nginx_metrics=False,
+        enable_statsd=False,
         environment="development",
+        files_world_accessible=True,
         hostname="Test hostname",
         log_level="trace",
         name="MyApp",
         push_api_key="some-api-key",
         revision="abc123",
+        running_in_container=True,
+        send_environment_metadata=True,
+        send_params=True,
+        send_session_data=True,
     )
 
     set_private_environ(config)
 
+    assert os.environ["_APPSIGNAL_ACTIVE"] == "true"
     assert os.environ["_APPSIGNAL_APP_ENV"] == "development"
     assert os.environ["_APPSIGNAL_APP_NAME"] == "MyApp"
     assert os.environ["_APPSIGNAL_APP_PATH"] == "/path/to/app"
+    assert os.environ["_APPSIGNAL_ENABLE_HOST_METRICS"] == "true"
+    assert os.environ["_APPSIGNAL_ENABLE_NGINX_METRICS"] == "false"
+    assert os.environ["_APPSIGNAL_ENABLE_STATSD"] == "false"
+    assert os.environ["_APPSIGNAL_FILES_WORLD_ACCESSIBLE"] == "true"
     assert os.environ["_APPSIGNAL_HOSTNAME"] == "Test hostname"
     assert os.environ["_APPSIGNAL_LOG_LEVEL"] == "trace"
     assert os.environ["_APPSIGNAL_PUSH_API_KEY"] == "some-api-key"
     assert (
         os.environ["_APPSIGNAL_LANGUAGE_INTEGRATION_VERSION"] == f"python-{__version__}"
     )
+    assert os.environ["_APPSIGNAL_RUNNING_IN_CONTAINER"] == "true"
+    assert os.environ["_APPSIGNAL_SEND_ENVIRONMENT_METADATA"] == "true"
+    assert os.environ["_APPSIGNAL_SEND_PARAMS"] == "true"
+    assert os.environ["_APPSIGNAL_SEND_SESSION_DATA"] == "true"
     assert os.environ["_APP_REVISION"] == "abc123"
+
+
+def test_set_private_environ_bool_is_none():
+    config = Options(
+        active=None,
+    )
+
+    set_private_environ(config)
+
+    assert os.environ["_APPSIGNAL_ACTIVE"] == ""
