7aefb080c783f1cff78ce72376642cf075f9dace
Tom de Bruijn <tom@tomdebruijn.com>
2023-05-24 09:27:13 +0200

    Fix "active" state in test
    Make the config actually something the agent can start with. Without the
    Push API key it doesn't start the agent and fails the test. Not sure how
    this only failed after merging #56.

--- tests/test_client.py
+++ tests/test_client.py
@@ -37,15 +37,18 @@ def test_client_active():
         active=True,
         name="MyApp",
         request_headers=["accept", "x-custom-header"],
+        push_api_key="0000-0000-0000-0000",
     )
     assert client._config.options["active"] is True
     assert client._config.options["name"] == "MyApp"
     assert client._config.options["request_headers"] == ["accept", "x-custom-header"]
+    assert client._config.options["push_api_key"] == "0000-0000-0000-0000"
     client.start()
 
     # Sets the private config environment variables
     assert os.environ.get("_APPSIGNAL_ACTIVE") == "true"
     assert os.environ.get("_APPSIGNAL_APP_NAME") == "MyApp"
+    assert os.environ.get("_APPSIGNAL_PUSH_API_KEY") == "0000-0000-0000-0000"
     assert (
         os.environ.get("OTEL_INSTRUMENTATION_HTTP_CAPTURE_HEADERS_SERVER_REQUEST")
         == "accept,x-custom-header"
