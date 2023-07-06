cba68d13191da0723ce44059355ec7680916bafa
Jeff Kreeftmeijer <jeff@kreeft.me>
2023-07-06 13:09:48 +0200

    Add PushApiKeyValidator
    Currently, validating push API keys is done in the install command. The
    upcoming diagnose command also needs to validate push API keys, so this
    patch moves the code into a separate module that will be used from both
    places.

---
+++ src/appsignal/push_api_key_validator.py
@@ -0,0 +1,22 @@
+import requests
+from appsignal.config import Config
+
+class PushApiKeyValidator:
+    @staticmethod
+    def validate(config: Config):
+        endpoint = config.option('endpoint')
+        url = f"{endpoint}/1/auth?api_key={config.option('push_api_key')}"
+        proxies = {}
+        if config.option("http_proxy"):
+            proxies["http"] = config.option("http_proxy")
+            proxies["https"] = config.option("http_proxy")
+
+        cert = config.option("ca_file_path")
+        response = requests.post(url, proxies=proxies, verify=cert)
+
+        if response.status_code == 200:
+            return 'valid'
+        elif response.status_code == 401:
+            return 'invalid'
+        else:
+            return response.status_code
