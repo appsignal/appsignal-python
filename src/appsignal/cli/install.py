d6743954b9686c92b9bcddd9e965262170b79beb
Jeff Kreeftmeijer <jeff@kreeft.me>
2023-07-06 13:29:23 +0200

    Use PushApiKeyValidator from install command
    The logic from the _calidate_push_api_key method was moved to the
    PushApiKeyValidator for reusability.

--- src/appsignal/cli/install.py
+++ src/appsignal/cli/install.py
@@ -7,6 +7,8 @@ from appsignal.config import Config
 from .command import AppsignalCLICommand
 from .demo import DemoCommand
 
+from appsignal.config import Config
+from appsignal.push_api_key_validator import PushApiKeyValidator
 
 INSTALL_FILE_TEMPLATE = """from appsignal import Appsignal
 
@@ -163,14 +165,4 @@ class InstallCommand(AppsignalCLICommand):
                 f.write(f"{dependency_name}\n")
 
     def _validate_push_api_key(self):
-        endpoint = self._config.option("endpoint")
-        url = f"{endpoint}/1/auth?api_key={self._push_api_key}"
-        proxies = {}
-        if self._config.option("http_proxy"):
-            proxies["http"] = self._config.option("http_proxy")
-            proxies["https"] = self._config.option("http_proxy")
-
-        cert = self._config.option("ca_file_path")
-
-        response = requests.get(url, proxies=proxies, verify=cert)
-        return response.status_code == 200
+        return PushApiKeyValidator.validate(self._config) == 'valid'
