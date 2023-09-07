d39ba6430a586f707514c586b75d8ea7e8c5491d
Noemi <45180344+unflxw@users.noreply.github.com>
2023-09-07 17:41:28 +0200

    Use in-memory metrics and spans in tests
    Use `mocker.patch()` to turn the functions that initialize
    OpenTelemetry as part of initializing the client into no-ops.
    
    Because the meter provider and the tracer provider are globals
    that can only be initialised once, use a session-scoped fixture to
    initialise them.
    
    In order to fetch them for use in a single test case, provide
    function-scoped fixtures that reset their state between usages.
    
    Implicitly fixes #88.

--- conftest.py
+++ conftest.py
@@ -6,8 +6,65 @@ import platform
 import tempfile
 
 import pytest
+from opentelemetry.metrics import set_meter_provider
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import InMemoryMetricReader
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+from opentelemetry.sdk.trace.export.in_memory_span_exporter import InMemorySpanExporter
+from opentelemetry.trace import set_tracer_provider
 
 from appsignal.agent import agent
+from appsignal.opentelemetry import METRICS_PREFERRED_TEMPORALITY
+
+
+@pytest.fixture(scope="function", autouse=True)
+def disable_start_opentelemetry(mocker):
+    mocker.patch("appsignal.opentelemetry._start_opentelemetry_tracer")
+    mocker.patch("appsignal.opentelemetry._start_opentelemetry_metrics")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def start_in_memory_metric_reader():
+    metric_reader = InMemoryMetricReader(
+        preferred_temporality=METRICS_PREFERRED_TEMPORALITY
+    )
+    provider = MeterProvider(resource=Resource({}), metric_readers=[metric_reader])
+    set_meter_provider(provider)
+
+    yield metric_reader
+
+
+@pytest.fixture(scope="session", autouse=True)
+def start_in_memory_span_exporter():
+    span_exporter = InMemorySpanExporter()
+    exporter_processor = SimpleSpanProcessor(span_exporter)
+    provider = TracerProvider()
+    provider.add_span_processor(exporter_processor)
+    set_tracer_provider(provider)
+
+    yield span_exporter
+
+
+@pytest.fixture(scope="function")
+def metrics(start_in_memory_metric_reader):
+    # Getting the metrics data implicitly wipes its state
+    start_in_memory_metric_reader.get_metrics_data()
+
+    yield start_in_memory_metric_reader.get_metrics_data
+
+
+@pytest.fixture(scope="function")
+def spans(start_in_memory_span_exporter):
+    start_in_memory_span_exporter.clear()
+
+    def get_and_clear_spans():
+        spans = start_in_memory_span_exporter.get_finished_spans()
+        start_in_memory_span_exporter.clear()
+        return spans
+
+    yield get_and_clear_spans
 
 
 @pytest.fixture(scope="function", autouse=True)
@@ -40,5 +97,3 @@ def stop_agent():
     working_dir = os.path.join(tmp_path, "appsignal")
     if os.path.isdir(working_dir):
         os.system(f"rm -rf {working_dir}")
-
-    yield
